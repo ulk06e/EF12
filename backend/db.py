18edc74ce5ed62a2d3aa3b2a4c021a88682e153a
Gleb <gleb.zuev04@gmail.com>
2025-06-19 10:57:54 +0300

    with postgres

--- backend/db.py
+++ backend/db.py
@@ -3,9 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 import enum
 import datetime
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
