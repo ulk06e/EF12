7113eec6b063c9f1a704dbd39456c5adf4f03051
Gleb <gleb.zuev04@gmail.com>
2025-06-19 17:26:47 +0300

    Temp

--- backend/db.py
+++ backend/db.py
@@ -4,8 +4,8 @@ import os
 from models import Day, Item, Project, TaskQualityEnum, ColumnLocationEnum, TimeQualityEnum
 from sqlalchemy.ext.declarative import declarative_base
 
+# ***REMOVED***
 ***REMOVED***
-
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
