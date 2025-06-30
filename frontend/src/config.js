bdf047f0343b54f6e069423e47169dc739bddb6c
Gleb Zuev <test@MacBook-Air-Gleb.local>
2025-06-30 11:25:24 +0300

    test

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,11 @@
 export const SETTINGS = {
-  API_URL_VERSION: 2, // 2 for local, 1 for server
+  API_URL_VERSION: 1, // 2 for local, 1 for server
   WEEK_SELECTOR: {
-    DURATION_LOW_MINUTES: 1200, // 4 hours
-    DURATION_MEDIUM_MINUTES: 1700, // 8 hours
+    DURATION_LOW_MINUTES: 1080, 
+    DURATION_MEDIUM_MINUTES: 1320, 
   },
   DASHBOARD: {
     REQUIRED_XP: 100,
-    REQUIRED_ACCOUNTED_TIME_MINUTES: 1200, // 8 hours
+    REQUIRED_ACCOUNTED_TIME_MINUTES: 1080, 
   },
 }; 
