3cbb27c28e83b9b85a95eb68c82f4eda660e0d2d
Gleb Zuev <test@MacBook-Air-Gleb.local>
2025-07-12 23:36:06 +0300

    test 2

--- frontend/src/shared/getApiUrl.js
+++ frontend/src/shared/getApiUrl.js
@@ -2,11 +2,12 @@ import { SETTINGS } from 'src/config';
 
 export function getApiUrl(env) {
     const urls = {
-      1: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+      1: import.meta.env.VITE_API_URL,
+      2: 'http://localhost:8000',
     };
-    return urls[env] || urls[1]; 
+    return urls[env] || urls[1]; // по умолчанию — прод
   }
   
-  export const API_URL = getApiUrl(SETTINGS.API_URL_VERSION); 
+  export const API_URL = getApiUrl(SETTINGS.API_URL_VERSION); // ← поменяй 1 или 2 здесь
 
   
